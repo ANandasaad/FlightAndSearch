4c30a0e3a41b0100608de6779d6b47874f21bbf2
Anand Kushwaha <98548977+ANandasaad@users.noreply.github.com>
2023-10-01 18:53:45 +0530

    Completed basic setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 import { PORT } from "./config/serverConfig.js";
 dotenv.config();
 const setupAndStartServer = async () => {
   const app = express();
   const port = PORT || 4000;
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });

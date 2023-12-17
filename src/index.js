c061360868bc44d07b5f16187d0b01f8f5a8fe13
Anand kushwaha <anandkushwaha@Anands-Mac-mini.local>
2023-12-17 19:32:06 +0530

    Flights,Airplanes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/index.js");
-const { PORT } = require("./config/serverConfig.js");
+const { PORT, SYNC_DB } = require("./config/serverConfig.js");
 const CityRepository = require("./respository/city-respository.js");
+const db = require("./models/index.js");
 
 const setupAndStartServer = async () => {
   const app = express();
   const port = PORT || 4000;
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
   app.use("/api", apiRoutes);
   app.listen(port, async () => {
     console.log(`Server listening on port ${port}`);
-    0;
+
+    // if (SYNC_DB) {
+    //   db.sequelize.sync({ alter: true });
+    // }
   });
 };
 
